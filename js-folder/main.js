eda990713f05df7261cf5ffae1c29b3be2e3222f
youdonghee <0103donghee@naver.com>
2023-04-17 14:19:46 +0900

    0417

--- js-folder/main.js
+++ js-folder/main.js
@@ -3,8 +3,8 @@ let min;
 let sec;
 let interval;
 let fast = false;
-let isModal=false;
-let count=0;
+let isModal = false;
+let count = 1;
 
 
 // 타이머
@@ -19,15 +19,16 @@ function reset(clearIntervalValue, interValue, timeoutValue, fastValue) {
 function setTimer(time) {
   if (!interval) {
     interval = setInterval(() => {
-      min = parseInt(timeout/60);
-      sec = timeout%60;
-      document.getElementById("time").innerHTML=`남은 시간 ${min} : ${sec}`
+      min = parseInt(timeout / 60);
+      sec = timeout % 60;
+      document.getElementById("time").innerHTML = `남은 시간 ${min} : ${sec}`
       timeout--;
       if (timeout < 0) {
-        document.getElementById("time").innerHTML=`시간종료`
-        reset(interval,null,null,false)
+        document.getElementById("time").innerHTML = `시간종료`
+        reset(interval, null, null, false)
         a();
         count++;
+        document.querySelector(".round").innerHTML = `ROUND ${count} `
         timer();
       }
     }, time);
@@ -35,12 +36,12 @@ function setTimer(time) {
 }
 
 function timer() {
-  if (interval == 1)return;
+  if (interval == 1) return;
   if (timeout == null) timeout = 10;
   setTimer(1000);
 }
 function stopTimer() {
-  reset(interval,null,timeout,false)
+  reset(interval, null, timeout, false)
   console.log("멈춰");
 }
 // function restart() {
@@ -48,8 +49,8 @@ function stopTimer() {
 //   console.log("다시");
 // }
 function ten() {
-  if(fast) return;
-  reset(interval,null,10,true)
+  if (fast) return;
+  reset(interval, null, 10, true)
   setTimer(1000);
 }
 const play = document.querySelector(".play");
@@ -72,22 +73,22 @@ let bb = document.querySelectorAll('.closeBtn')
 // console.log(aa); // A f, B f 담기고
 
 let open = function (i) {
-  if(isModal){
+  if (isModal) {
 
-  }else{
-    isModal=true
+  } else {
+    isModal = true
     let a = document.querySelectorAll(".modal");
     console.log(a);
     a[i].classList.remove("hidden");
   }
 }
 // 클릭
-aa.forEach(function(i,index){
-  i.onclick = function(){
+aa.forEach(function (i, index) {
+  i.onclick = function () {
     open(index);
   }
 })
-bb.forEach(function (i,index) {
+bb.forEach(function (i, index) {
   i.onclick = function () {
     close(index);
   }
@@ -97,9 +98,9 @@ bb.forEach(function (i,index) {
 let close = function (i) {
   let a = document.querySelectorAll(".modal");
   a[i].classList.add("hidden");
-  setTimeout(()=>{
-    isModal=false
-  },10)
+  setTimeout(() => {
+    isModal = false
+  }, 10)
 }
 
 // aa.forEach(function(i,index){
@@ -108,10 +109,10 @@ let close = function (i) {
 //   }
 // })
 
-  
-  // document.querySelector(".A").addEventListener("click", open(0));
-  // document.querySelector(".B").addEventListener("click", open(i));
-  
+
+// document.querySelector(".A").addEventListener("click", open(0));
+// document.querySelector(".B").addEventListener("click", open(i));
+
 // document.querySelector(".C").addEventListener("click", open);
 // document.querySelector(".D").addEventListener("click", open);
 // document.querySelector(".E").addEventListener("click", open);
@@ -119,28 +120,50 @@ document.querySelector(".closeBtn").addEventListener("click", close);
 
 // document.querySelector(".bg").addEventListener("click", close);
 
-// // 매수 매도 값 보내기
-// let buyNumArr = [0, 0]; // 총수량
-// let wallet = 10000;
-// wallet = document.querySelector(".rest-money").innerHTML = `내 보유현금 : ${wallet} 원`
-// let text = document.querySelectorAll(".vol")
+// **추가 된 내용
 
-// function getvalueInText(i) {
-//   let inputData = document.getElementsByClassName("data")[i].value;
+// 뉴스속보
+// 팝업 내용을 담은 배열
+const popupContents = [
+  "Level 1 cleared!",
+  "You have a new message!",
+  "Game over! Try again?",
+  "You found a hidden treasure!"
+];
 
-//   if (inputData <= 0) {
-//     alert("1이상의 숫자만 넣어주세요")
-//   }
-//   else{
-//     buyNumArr[i] += parseInt(inputData);
-//     console.log(buyNumArr);
-//     text[i + 1].innerHTML=buyNumArr[i]
-//   }
-//   // 인풋밸류 담기
-// }
-// // 매수 조건 1. 1이상만 가능 2. 보유현금 보다 이상으로 구매할수없음
-// // 구매하려면 : 내 현금 > 현재가*수량 
+// 팝업 애니메이션 지속시간 
+const popupDuration = 5000;
 
-// function wall() {
-//   wallet
-// }
+// 팝업 반복 시간 (10초) 6분은 (360000)
+let popTime = 10000;
+
+// 팝업 생성 함수
+function createPopup() {
+  // 랜덤으로 팝업 내용 선택
+  const randomIndex = Math.floor(Math.random() * popupContents.length);
+  const content = popupContents[randomIndex];
+
+  // 팝업 요소 생성
+  const popup = document.createElement("div"); //div 만들고
+  popup.classList.add("popup"); // 클래스 이름 popup
+  popup.textContent = content; // textContent : text 콘텐츠 계속 변경
+
+  // 팝업을 body 요소에 추가
+  document.body.appendChild(popup);
+
+  // 팝업 애니메이션 시작
+  setTimeout(() => {
+    popup.classList.add("show");
+  }, 100);
+
+  // 팝업 삭제
+  setTimeout(() => {
+    popup.classList.remove("show");
+    setTimeout(() => {
+      document.body.removeChild(popup);
+    }, 1000);
+  }, popupDuration);
+}
+
+// 주기적으로 팝업 생성
+setInterval(createPopup, popTime);
--- js-folder/main2.js
+++ js-folder/main2.js
@@ -1,6 +1,6 @@
 let nowPrice = document.querySelectorAll(".now-price");
 let howPrice = document.querySelectorAll(".how-price");
-let sumA=[2000,2000,2000,2000,2000];
+let sumA = [2000, 2000, 2000, 2000, 2000];
 let icon = document.querySelectorAll(".icon");
 let imgA = document.querySelector(".A");
 let imgB = document.querySelector(".B");
@@ -10,9 +10,9 @@ let imgE = document.querySelector(".E");
 let imgX = document.querySelectorAll(".x");
 // let iconPlus= document.querySelector(".iconPlus");
 // let iconMinus= document.querySelector(".iconMinus");
-let pm =["iconPlus","iconMinus"];
-let bool= [true,true,true,true,true];
-let images = ['../img/경일상폐.jpg','../img/경일상폐1.jpg','../img/경일상폐2.jpg','../img/경일상폐3.jpg','../img/경일상폐4.jpg']
+let pm = ["iconPlus", "iconMinus"];
+let bool = [true, true, true, true, true];
+let images = ['../img/경일상폐.jpg', '../img/경일상폐1.jpg', '../img/경일상폐2.jpg', '../img/경일상폐3.jpg', '../img/경일상폐4.jpg']
 let list = document.querySelectorAll(".list");
 let listPrice = document.querySelectorAll(".list-flex-price");
 let listPer = document.querySelectorAll(".list-flex-per");
@@ -21,12 +21,12 @@ let randomNumber;
 let btn1 = document.querySelectorAll(".btn1");
 let btn2 = document.querySelectorAll(".btn2");
 
-let avgNummArr = [0, 0, 0, 0,0]; // 평균단가
-let buyNumArr = [0, 0, 0, 0,0]; // 총수량
-let priNumArr = [0, 0, 0, 0,0]; // 총매입가격
-let plusArr = [0, 0, 0, 0,0]; // 평가손익
-let percentArr = [0, 0, 0, 0,0]; // 수익률
-let allMonArr = [0, 0, 0, 0,0]; // 총평가금액
+let avgNummArr = [0, 0, 0, 0, 0]; // 평균단가
+let buyNumArr = [0, 0, 0, 0, 0]; // 총수량
+let priNumArr = [0, 0, 0, 0, 0]; // 총매입가격
+let plusArr = [0, 0, 0, 0, 0]; // 평가손익
+let percentArr = [0, 0, 0, 0, 0]; // 수익률
+let allMonArr = [0, 0, 0, 0, 0]; // 총평가금액
 
 let wallet = document.querySelector(".rest-money") // 보유현금
 let avg = document.querySelectorAll(".avg-price") // 평균단가
@@ -39,160 +39,159 @@ let money = 10000;
 
 // console.log(icon);
 // console.log(imgAll);
-function randomNum(){
+function randomNum() {
     // 랜덤값 ( howPrice, 상승&하락 결정 수)
-    let a = Math.floor(Math.random()*1100);
+    let a = Math.floor(Math.random() * 1100);
 
-    if (a>100 && a<200)
-        a=100;
-    else if(a<100)
-        a=0;
-    else if(a>200 && a<300)
-        a=200;
-    else if(a>300 && a<400)
-        a=300;
-    else if(a>400 && a<500)
-        a=400;
-    else if(a>500 && a<600)
-        a=500;
-    else if(a>600 && a<700)
-        a=600;
-    else if(a>700 && a<800)
-        a=700;
-    else if(a>800 && a<900)
-        a=800;
-    else if(a>900 && a<1000)
-        a=900;
-    else if(a>1000)
-        a=1000;
-    
+    if (a > 100 && a < 200)
+        a = 100;
+    else if (a < 100)
+        a = 0;
+    else if (a > 200 && a < 300)
+        a = 200;
+    else if (a > 300 && a < 400)
+        a = 300;
+    else if (a > 400 && a < 500)
+        a = 400;
+    else if (a > 500 && a < 600)
+        a = 500;
+    else if (a > 600 && a < 700)
+        a = 600;
+    else if (a > 700 && a < 800)
+        a = 700;
+    else if (a > 800 && a < 900)
+        a = 800;
+    else if (a > 900 && a < 1000)
+        a = 900;
+    else if (a > 1000)
+        a = 1000;
+
     return a;
 }
-function stay(i){
-    sumA[i] = sumA[i]-randomNumber;
+function stay(i) {
+    sumA[i] = sumA[i] - randomNumber;
     nowPrice[i].innerHTML = sumA[i];
     howPrice[i].style.color = "gray";
-    howPrice[i].innerHTML=randomNumber;
+    howPrice[i].innerHTML = randomNumber;
     icon[i].className = "icon"
-    
+
 }
 
-function down(i){
-    sumA[i] = sumA[i]-randomNumber;
-        nowPrice[i].innerHTML = sumA[i];
-        howPrice[i].style.color = "blue";
-        howPrice[i].innerHTML=randomNumber;
-        // console.log(pm[1]);
-        icon[i].className = "icon "+pm[1];
+function down(i) {
+    sumA[i] = sumA[i] - randomNumber;
+    nowPrice[i].innerHTML = sumA[i];
+    howPrice[i].style.color = "blue";
+    howPrice[i].innerHTML = randomNumber;
+    // console.log(pm[1]);
+    icon[i].className = "icon " + pm[1];
 }
 
-function up(i){
-    sumA[i] = sumA[i]+randomNumber;
-        nowPrice[i].innerHTML = sumA[i];
-        howPrice[i].style.color = "red";
-        howPrice[i].innerHTML=randomNumber;
-        // console.log(pm[0]);
-        icon[i].className = "icon "+pm[0];
+function up(i) {
+    sumA[i] = sumA[i] + randomNumber;
+    nowPrice[i].innerHTML = sumA[i];
+    howPrice[i].style.color = "red";
+    howPrice[i].innerHTML = randomNumber;
+    // console.log(pm[0]);
+    icon[i].className = "icon " + pm[0];
 }
 
-function finish(i){
+function finish(i) {
     switch (i) {
         case 0:
             // imgA.style.backgroundImage = `url(${images[i]})`;
             imgA.style.backgroundColor = '#000000cc';
-            imgX[i].style.display="block"; 
+            imgX[i].style.display = "block";
             break;
         case 1:
             imgB.style.backgroundColor = '#000000cc';
-            imgX[i].style.display="block";
+            imgX[i].style.display = "block";
             break;
         case 2:
             imgC.style.backgroundColor = '#000000cc';
-            imgX[i].style.display="block";
+            imgX[i].style.display = "block";
             break;
         case 3:
             imgD.style.backgroundColor = '#000000cc';
-            imgX[i].style.display="block";
+            imgX[i].style.display = "block";
             break;
         case 4:
             imgE.style.backgroundColor = '#000000cc';
-            imgX[i].style.display="block";
+            imgX[i].style.display = "block";
             break;
     }
-    
-              
+
+
     // 해야될거 이미지 배열로 하기
-            
-            setTimeout(() => {
-                howPrice[i].style.color = "gray";
-                howPrice[i].innerHTML= "-";
-                icon[i].className = "icon"
-                nowPrice[i].innerHTML = "-";
-            }, 1000);
+
+    setTimeout(() => {
+        howPrice[i].style.color = "gray";
+        howPrice[i].innerHTML = "-";
+        icon[i].className = "icon"
+        nowPrice[i].innerHTML = "-";
+    }, 1000);
 }
 
 // 라운드마다 가격변동
-function randomPrice(){
-    let i=0;
-    for(i ; i < 5 ; i++){
-    if(bool[i]==true){
+function randomPrice() {
+    let i = 0;
+    for (i; i < 5; i++) {
+        if (bool[i] == true) {
 
-        randomNumber = randomNum();
-        // console.log(randomNumber);
-
-    //하락
-    let b = Math.floor(Math.random()*2);
-    if(b==0){ 
-
-        if(randomNumber==0){ 
+            randomNumber = randomNum();
             // console.log(randomNumber);
-            stay(i);
-        }
-        // 하락일 때
-        else{
-            // 하락인데 sumA가 하락금액보다 낮을 때
-        // console.log(randomNumber);
-            if(sumA[i]<randomNumber)
-            {
-                randomNumber=sumA[i];
+
+            //하락
+            let b = Math.floor(Math.random() * 2);
+            if (b == 0) {
+
+                if (randomNumber == 0) {
+                    // console.log(randomNumber);
+                    stay(i);
+                }
+                // 하락일 때
+                else {
+                    // 하락인데 sumA가 하락금액보다 낮을 때
+                    // console.log(randomNumber);
+                    if (sumA[i] < randomNumber) {
+                        randomNumber = sumA[i];
+                    }
+                    down(i);
+                }
+                // 0원이 되었을 때
+                if (sumA[i] == 0) {
+
+                    bool[i] = false;
+                    finish(i);
+                }
+
             }
-            down(i);
-        }
-        // 0원이 되었을 때
-        if(sumA[i]==0){
-            
-            bool[i]=false;
-            finish(i);
-        }
-       
-    }
-    // 상승
-    if(b==1){
+            // 상승
+            if (b == 1) {
 
-        // 등락률이 0일 때
-        if(randomNumber==0){
-           stay(i);
-        }
-       else{
-            up(i);
- 
-        } 
-    }
-    }       
-    }
-    let arr=[];    
+                // 등락률이 0일 때
+                if (randomNumber == 0) {
+                    stay(i);
+                }
+                else {
+                    up(i);
 
-        arr.push(sumA[0]);
-        // console.log(sumA[0]);
-        arr.push(sumA[1]);
-        arr.push(sumA[2]);
-        arr.push(sumA[3]);
-        arr.push(sumA[4]);
+                }
+            }
+        }
+    }
+    let arr = [];
+
+    arr.push(sumA[0]);
+    // console.log(sumA[0]);
+    arr.push(sumA[1]);
+    arr.push(sumA[2]);
+    arr.push(sumA[3]);
+    arr.push(sumA[4]);
 
     return arr;
-    
+
 }
-let arr=new Array(5).fill(0);
+let arr = new Array(5).fill(0);
 
 // 주식 상승률 순위 js
 function a() {
@@ -202,60 +201,60 @@ function a() {
 
     // let map = new Map();
 
-    let arr2 = [{name: "KI학원", value : arr[0]},
-                {name: "CM건설", value : arr[1]},
-                {name: "JW은행", value : arr[2]},
-                {name: "JY전자", value : arr[3]},
-                {name: "DH통신", value : arr[4]}]
+    let arr2 = [{ name: "KI학원", value: arr[0] },
+    { name: "CM건설", value: arr[1] },
+    { name: "JW은행", value: arr[2] },
+    { name: "JY전자", value: arr[3] },
+    { name: "DH통신", value: arr[4] }]
 
-    arr2.sort(function(a,b){
-    
+    arr2.sort(function (a, b) {
+
         return b.value - a.value;
     })
- 
-    list.forEach((e,i)=>{
+
+    list.forEach((e, i) => {
         e.innerHTML = arr2[i].name;
 
-        if(arr2[i].value<2000){
-            e.style.color ="blue";
+        if (arr2[i].value < 2000) {
+            e.style.color = "blue";
         }
-        else if(arr2[i].value==2000){
-            e.style.color ="black";
+        else if (arr2[i].value == 2000) {
+            e.style.color = "black";
         }
-        else if(arr2[i].value>2000){
-            e.style.color ="red";
+        else if (arr2[i].value > 2000) {
+            e.style.color = "red";
         }
     })
 
-    listPrice.forEach((e,i)=>{
-        
+    listPrice.forEach((e, i) => {
+
         e.innerHTML = arr2[i].value;
 
-        if(arr2[i].value<2000){
-            e.style.color ="blue";
+        if (arr2[i].value < 2000) {
+            e.style.color = "blue";
         }
-        else if(arr2[i].value==2000){
-            e.style.color ="black";
+        else if (arr2[i].value == 2000) {
+            e.style.color = "black";
         }
-        else if(arr2[i].value>2000){
-            e.style.color ="red";
+        else if (arr2[i].value > 2000) {
+            e.style.color = "red";
         }
     });
 
-    listPer.forEach((e,i)=>{
+    listPer.forEach((e, i) => {
         // let plusNum;
-        if((2000-arr2[i].value)<0){
-            e.innerHTML = "+"+-parseInt(((2000-arr2[i].value) / 2000) *100) + "%";
-            e.style.color ="red";
+        if ((2000 - arr2[i].value) < 0) {
+            e.innerHTML = "+" + -parseInt(((2000 - arr2[i].value) / 2000) * 100) + "%";
+            e.style.color = "red";
         }
-        else if((2000-arr2[i].value)>0){
-             e.innerHTML = -parseInt(((2000-arr2[i].value) / 2000) *100) + "%";
-             e.style.color ="blue";
-        // }}}
+        else if ((2000 - arr2[i].value) > 0) {
+            e.innerHTML = -parseInt(((2000 - arr2[i].value) / 2000) * 100) + "%";
+            e.style.color = "blue";
+            // }}}
         }
-        else if((2000-arr2[i].value)==0){
+        else if ((2000 - arr2[i].value) == 0) {
             e.innerHTML = "0%";
-             e.style.color ="black";
+            e.style.color = "black";
         }
         // else if((2000-arr2[i].value)>0){
 
@@ -270,64 +269,66 @@ function a() {
     //     setvalueInText(i);
 
     // })
-    
-    for (let i = 0; i <5; i++) {
-        
-     // 평가손익 (보유중인 주식의 현재가격 * 수량) - 매입총액 = 평가손익
-     plusArr[i] = (nowPrice[i].innerHTML * buyNumArr[i]) - priNumArr[i]
-     plus[i + 1].innerHTML = plusArr[i];
 
-     // 수익률 (현재가 - 평균매입가격 / 평균매입가격) * 100 = 수익률 
-     percentArr[i] = ((nowPrice[i].innerHTML - avgNummArr[i]) / avgNummArr[i]) * 100
-     plus[i + 1].innerHTML = `${plusArr[i]} (${percentArr[i].toFixed(2)} %)`// 소수점 2자리 까지 : toFixed(2)
+    for (let i = 0; i < 5; i++) {
 
-     // 총평가금액
-     allMonArr[i] = (priNumArr[i] + plusArr[i])
-     allMon[i + 1].innerHTML = allMonArr[i];
-     console.log(allMonArr[i]);
+        // 평가손익 (보유중인 주식의 현재가격 * 수량) - 매입총액 = 평가손익
+        plusArr[i] = (nowPrice[i].innerHTML * buyNumArr[i]) - priNumArr[i]
+        plus[i + 1].innerHTML = plusArr[i];
 
-      // 평가손익 (보유중인 주식의 현재가격 * 수량) - 매입총액 = 평가손익
-      plusArr[i] = (nowPrice[i].innerHTML * buyNumArr[i]) - priNumArr[i]
-      plus[i + 1].innerHTML = plusArr[i];
+        // 수익률 (현재가 - 평균매입가격 / 평균매입가격) * 100 = 수익률 
+        percentArr[i] = ((nowPrice[i].innerHTML - avgNummArr[i]) / avgNummArr[i]) * 100
+        plus[i + 1].innerHTML = `${plusArr[i]} (${percentArr[i].toFixed(2)} %)`// 소수점 2자리 까지 : toFixed(2)
 
-      // 수익률 (현재가 - 평균매입가격 / 평균매입가격) * 100 = 수익률 
-      if (avgNummArr[i] !== 0) { //분모가 0일때 무한으로 출력이됨 방지해줌
-          percentArr[i] = ((nowPrice[i].innerHTML - avgNummArr[i]) / avgNummArr[i]) * 100
-          plus[i + 1].innerHTML = `${plusArr[i]} (${percentArr[i].toFixed(2)} %)`// 소수점 2자리 까지 : toFixed(2)
-      } else {
-          percentArr[i] = 0;
-      }
+        // 총평가금액
+        allMonArr[i] = (priNumArr[i] + plusArr[i])
+        allMon[i + 1].innerHTML = allMonArr[i];
+        console.log(allMonArr[i]);
 
-      // 총평가금액
-      allMonArr[i] = (priNumArr[i] + plusArr[i])
-      allMon[i + 1].innerHTML = allMonArr[i];
+
+        // 평가손익 (보유중인 주식의 현재가격 * 수량) - 매입총액 = 평가손익
+        plusArr[i] = (nowPrice[i].innerHTML * buyNumArr[i]) - priNumArr[i]
+        plus[i + 1].innerHTML = plusArr[i];
+
+        // 수익률 (현재가 - 평균매입가격 / 평균매입가격) * 100 = 수익률 
+        if (avgNummArr[i] !== 0) { //분모가 0일때 무한으로 출력이됨 방지해줌
+            percentArr[i] = (Number(nowPrice[i].innerHTML - avgNummArr[i]) / avgNummArr[i]) * 100
+
+            plus[i + 1].innerHTML = `${plusArr[i]} (${percentArr[i].toFixed(2)} %)`// 소수점 2자리 까지 : toFixed(2)
+        } else {
+            percentArr[i] = 0;
+        }
+
+        // 총평가금액
+        allMonArr[i] = (priNumArr[i] + plusArr[i])
+        allMon[i + 1].innerHTML = allMonArr[i];
 
     }
 
     //등락률 ((2000-현재가) / 2000) *100
 }
-    // map.set('KI학원', arr[0]);
-    // map.set('CM건설', arr[1]);
-    // map.set('JW은행', arr[2]);
-    // map.set('JY전자', arr[3]);
-    // map.set('DH통신', arr[4]);
+// map.set('KI학원', arr[0]);
+// map.set('CM건설', arr[1]);
+// map.set('JW은행', arr[2]);
+// map.set('JY전자', arr[3]);
+// map.set('DH통신', arr[4]);
 
-    // for (let key of map.keys()) {
-    //     console.log(key + " / " + map.get(key) ) 
-    //   } 
-    
-    // arr.sort(function(a, b) { // 내림차순 정렬
-    //     // console.log(a);
-    //     return b - a ;
-    // });
+// for (let key of map.keys()) {
+//     console.log(key + " / " + map.get(key) ) 
+//   } 
 
-    // for (let key of map.keys()) {
-    //     console.log(key + " / " + map.get(key) ) 
-    //   } 
+// arr.sort(function(a, b) { // 내림차순 정렬
+//     // console.log(a);
+//     return b - a ;
+// });
+
+// for (let key of map.keys()) {
+//     console.log(key + " / " + map.get(key) ) 
+//   } 
 
 //     console.log(arr);
 //  for(let i=0; i <5 ; i++){
-        
+
 //     if(arr[i]==map.get('KI학원')){
 //         list[i].innerHTML = "KI학원"; 
 //     }
@@ -392,17 +393,22 @@ function getvalueInText(i) {
         plus[i + 1].innerHTML = plusArr[i];
 
         // 수익률 (현재가 - 평균매입가격 / 평균매입가격) * 100 = 수익률 
-        percentArr[i] = ((nowPrice[i].innerHTML - avgNummArr[i]) / avgNummArr[i]) * 100
+        percentArr[i] = (Number(nowPrice[i].innerHTML - avgNummArr[i]) / avgNummArr[i]) * 100
         plus[i + 1].innerHTML = `${plusArr[i]} (${percentArr[i].toFixed(2)} %)`// 소수점 2자리 까지 : toFixed(2)
 
         // 총평가금액
         allMonArr[i] = (priNumArr[i] + plusArr[i])
         allMon[i + 1].innerHTML = allMonArr[i];
         console.log(allMonArr[i]);
+        document.getElementsByClassName("data")[i].value = ""
+        alert(`${inputData} 개 매수 하였습니다`)
     }
     else {
+        document.getElementsByClassName("data")[i].value = ""
         alert("돈 없어")
     }
+    console.log("dd");
+    close(i);
 }
 // 평가손익, 총평가금액은 매수매도 했을대도 바꿔지고 라운드가 끝났을때도 바꿔주기
 
@@ -446,8 +452,8 @@ function setvalueInText(i) {
         // 총 매입가격 (총 매입가격 - 현재가격)
         priNumArr[i] -= nowPrice[i].innerHTML * inputData;
         // ***수정
-        if (buyNumArr[i] ==0) {
-            priNumArr[i] =0;
+        if (buyNumArr[i] == 0) {
+            priNumArr[i] = 0;
         }
 
         buyPrice[i + 1].innerHTML = priNumArr[i]
@@ -476,13 +482,17 @@ function setvalueInText(i) {
         // 총평가금액
         allMonArr[i] = (priNumArr[i] + plusArr[i])
         allMon[i + 1].innerHTML = allMonArr[i];
+        document.getElementsByClassName("data")[i].value = ""
+        alert(`${inputData} 개 매도 하였습니다`)
     }
     else {
-        alert("돈 없어") 
+        alert("돈 없어")
     }
+    close(i);
 }
 
 
 // 오류 
 // 상폐됐을떄 NaN 처리해주기
+
 // 매수 매도 했을때 닫아주기
