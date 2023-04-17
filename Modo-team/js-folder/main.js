6597542f3ed4417ec7bf3d6f1a5831dc850e238c
zam0ng <127164530+zam0ng@users.noreply.github.com>
2023-04-17 10:10:06 +0900

    1
    1

--- Modo-team/js-folder/main.js
+++ Modo-team/js-folder/main.js
@@ -3,7 +3,9 @@ let min;
 let sec;
 let interval;
 let fast = false;
+let isModal=false;
 let count=0;
+
 
 // 타이머
 function reset(clearIntervalValue, interValue, timeoutValue, fastValue) {
@@ -11,6 +13,7 @@ function reset(clearIntervalValue, interValue, timeoutValue, fastValue) {
   interval = interValue;
   timeout = timeoutValue;
   fast = fastValue;
+
 }
 
 function setTimer(time) {
@@ -22,15 +25,10 @@ function setTimer(time) {
       timeout--;
       if (timeout < 0) {
         document.getElementById("time").innerHTML=`시간종료`
-        reset(interval,null,null,false);
+        reset(interval,null,null,false)
         a();
         count++;
-        ab();
         timer();
-        // a();
-
-        // console.log(sumA[i]);
-
       }
     }, time);
   }
@@ -52,7 +50,7 @@ function stopTimer() {
 function ten() {
   if(fast) return;
   reset(interval,null,10,true)
-  setTimer(200);
+  setTimer(1000);
 }
 const play = document.querySelector(".play");
 const timeStop = document.querySelector(".stop");
@@ -63,58 +61,86 @@ timeStop.onclick = stopTimer;
 skip.onclick = ten;
 
 // 뉴스
-// let target = document.querySelector(".news");
-// let player = function player() {
-//   target.animate(
-//     [
-//       {
-//         transform : 'translateY(30px)',
-//         opacity: '0'
-//       },
-//       {
-//         transform : 'translate(0,0)',
-//         opacity: '1'
-//       }
-//     ], 1000
-//   )
-// }
 // 라운드 5분마다 팝업 1개씩 띄우기
 // 팝업은 10초후 사라지기
 
-// 매입 매도
-// 눌렀을때 모달팝업 뜨고
-// let target = document.querySelector(".news");
-// let player = function player() {
-//   target.animate(
-//     [
-//       {
-//         transform : 'translateY(30px)',
-//         opacity: '0'
-//       },
-//       {
-//         transform : 'translate(0,0)',
-//         opacity: '1'
-//       }
-//     ], 1000
-//   )
-// }
-// 라운드 5분마다 팝업 1개씩 띄우기
-// 팝업은 10초후 사라지기
+
 
 // 매입 매도
-// 눌렀을때 모달팝업 뜨고
-let open = function () {
-  document.querySelector(".modal").classList.remove("hidden");
+let aa = document.querySelectorAll('.f');
+let bb = document.querySelectorAll('.closeBtn')
+// console.log(aa); // A f, B f 담기고
+
+let open = function (i) {
+  if(isModal){
+
+  }else{
+    isModal=true
+    let a = document.querySelectorAll(".modal");
+    console.log(a);
+    a[i].classList.remove("hidden");
+  }
 }
-let close = function () {
-  document.querySelector(".modal").classList.add("hidden");
+// 클릭
+aa.forEach(function(i,index){
+  i.onclick = function(){
+    open(index);
+  }
+})
+bb.forEach(function (i,index) {
+  i.onclick = function () {
+    close(index);
+  }
+})
+
+
+let close = function (i) {
+  let a = document.querySelectorAll(".modal");
+  a[i].classList.add("hidden");
+  setTimeout(()=>{
+    isModal=false
+  },10)
 }
-document.querySelector(".A").addEventListener("click", open);
+
+// aa.forEach(function(i,index){
+//   i.onclick = function(){
+//     close(index)
+//   }
+// })
+
+  
+  // document.querySelector(".A").addEventListener("click", open(0));
+  // document.querySelector(".B").addEventListener("click", open(i));
+  
+// document.querySelector(".C").addEventListener("click", open);
+// document.querySelector(".D").addEventListener("click", open);
+// document.querySelector(".E").addEventListener("click", open);
 document.querySelector(".closeBtn").addEventListener("click", close);
-document.querySelector(".bg").addEventListener("click", close);
 
-// 값 보내기
-function getvalueInText(i) {
-  let inputData = document.getElementById("data1").value;
-  let text = document.querySelectorAll(".vol")[i].innerHTML=inputData
-}
+// document.querySelector(".bg").addEventListener("click", close);
+
+// // 매수 매도 값 보내기
+// let buyNumArr = [0, 0]; // 총수량
+// let wallet = 10000;
+// wallet = document.querySelector(".rest-money").innerHTML = `내 보유현금 : ${wallet} 원`
+// let text = document.querySelectorAll(".vol")
+
+// function getvalueInText(i) {
+//   let inputData = document.getElementsByClassName("data")[i].value;
+
+//   if (inputData <= 0) {
+//     alert("1이상의 숫자만 넣어주세요")
+//   }
+//   else{
+//     buyNumArr[i] += parseInt(inputData);
+//     console.log(buyNumArr);
+//     text[i + 1].innerHTML=buyNumArr[i]
+//   }
+//   // 인풋밸류 담기
+// }
+// // 매수 조건 1. 1이상만 가능 2. 보유현금 보다 이상으로 구매할수없음
+// // 구매하려면 : 내 현금 > 현재가*수량 
+
+// function wall() {
+//   wallet
+// }
--- Modo-team/js-folder/main2.js
+++ Modo-team/js-folder/main2.js
@@ -17,6 +17,26 @@ let list = document.querySelectorAll(".list");
 let listPrice = document.querySelectorAll(".list-flex-price");
 let listPer = document.querySelectorAll(".list-flex-per");
 let randomNumber;
+
+let btn1 = document.querySelectorAll(".btn1");
+let btn2 = document.querySelectorAll(".btn2");
+
+let avgNummArr = [0, 0, 0, 0,0]; // 평균단가
+let buyNumArr = [0, 0, 0, 0,0]; // 총수량
+let priNumArr = [0, 0, 0, 0,0]; // 총매입가격
+let plusArr = [0, 0, 0, 0,0]; // 평가손익
+let percentArr = [0, 0, 0, 0,0]; // 수익률
+let allMonArr = [0, 0, 0, 0,0]; // 총평가금액
+
+let wallet = document.querySelector(".rest-money") // 보유현금
+let avg = document.querySelectorAll(".avg-price") // 평균단가
+let text = document.querySelectorAll(".vol") // 총수량
+let buyPrice = document.querySelectorAll(".buy-price") // 총매입가격
+let plus = document.querySelectorAll(".plus-minus") // 평가손익 (수익률)
+let allMon = document.querySelectorAll(".all-money") //총평가금액
+
+let money = 10000;
+
 // console.log(icon);
 // console.log(imgAll);
 function randomNum(){
@@ -173,12 +193,18 @@ function randomPrice(){
     
 }
 let arr=new Array(5).fill(0);
+window.localStorage.clear();
 
 // 주식 상승률 순위 js
 function a() {
     // console.log(sumA[0]);
     arr = randomPrice();
     window.localStorage.setItem("KI학원", sumA[0]);
+    window.localStorage.setItem("CM건설", sumA[1]);
+    window.localStorage.setItem("JW은행", sumA[2]);
+    window.localStorage.setItem("JY전자", sumA[3]);
+    window.localStorage.setItem("DH통신", sumA[4]);
+
 
     // let map = new Map();
 
@@ -242,6 +268,48 @@ function a() {
         // }
     });
 
+    // btn1.forEach(function(e,i){
+    //     getvalueInText(i);
+    // })
+
+    // btn2.forEach(function(e,i){
+    //     setvalueInText(i);
+
+    // })
+    
+    for (let i = 0; i <5; i++) {
+        
+     // 평가손익 (보유중인 주식의 현재가격 * 수량) - 매입총액 = 평가손익
+     plusArr[i] = (nowPrice[i].innerHTML * buyNumArr[i]) - priNumArr[i]
+     plus[i + 1].innerHTML = plusArr[i];
+
+     // 수익률 (현재가 - 평균매입가격 / 평균매입가격) * 100 = 수익률 
+     percentArr[i] = ((nowPrice[i].innerHTML - avgNummArr[i]) / avgNummArr[i]) * 100
+     plus[i + 1].innerHTML = `${plusArr[i]} (${percentArr[i].toFixed(2)} %)`// 소수점 2자리 까지 : toFixed(2)
+
+     // 총평가금액
+     allMonArr[i] = (priNumArr[i] + plusArr[i])
+     allMon[i + 1].innerHTML = allMonArr[i];
+    //  console.log(allMonArr[i]);
+
+      // 평가손익 (보유중인 주식의 현재가격 * 수량) - 매입총액 = 평가손익
+      plusArr[i] = (nowPrice[i].innerHTML * buyNumArr[i]) - priNumArr[i]
+      plus[i + 1].innerHTML = plusArr[i];
+
+      // 수익률 (현재가 - 평균매입가격 / 평균매입가격) * 100 = 수익률 
+      if (avgNummArr[i] !== 0) { //분모가 0일때 무한으로 출력이됨 방지해줌
+          percentArr[i] = ((nowPrice[i].innerHTML - avgNummArr[i]) / avgNummArr[i]) * 100
+          plus[i + 1].innerHTML = `${plusArr[i]} (${percentArr[i].toFixed(2)} %)`// 소수점 2자리 까지 : toFixed(2)
+      } else {
+          percentArr[i] = 0;
+      }
+
+      // 총평가금액
+      allMonArr[i] = (priNumArr[i] + plusArr[i])
+      allMon[i + 1].innerHTML = allMonArr[i];
+
+    }
+
     //등락률 ((2000-현재가) / 2000) *100
 }
     // map.set('KI학원', arr[0]);
@@ -283,6 +351,163 @@ function a() {
 //     }
 // }
 
+// 매수 매도 값 보내기
+
+wallet.innerHTML = `내 보유현금 : ${money} 원`
+
+// 매수
+function getvalueInText(i) {
+
+    // 함수로 만들기
+    let inputData = document.getElementsByClassName("data")[i].value;
+    if (inputData <= 0) {
+        alert("1이상의 숫자만 넣어주세요")
+        console.log(inputNum.value);
+        document.getElementsByClassName("data")[i].value = ""
+    } else if (!Number.isInteger(parseFloat(inputData))) { //Number.isInteger() 함수는 입력된 값이 정수인지를 체크 // parseFloat() 함수는 문자열을 부동소수점 수로 변환
+        alert("소수점 안돼")
+        document.getElementsByClassName("data")[i].value = ""
+    }
+
+
+    else if (money >= nowPrice[i].innerHTML * inputData) {
+        // 보유현금
+        money = money - (nowPrice[i].innerHTML * inputData)
+        wallet.innerHTML = `내 보유현금 : ${money} 원`
+
+        // 수량
+        buyNumArr[i] += Number(inputData);
+        text[i + 1].innerHTML = buyNumArr[i]
+
+        // 총 매입가격
+        priNumArr[i] += nowPrice[i].innerHTML * inputData;
+        buyPrice[i + 1].innerHTML = priNumArr[i]
+
+        // 평균단가 총매입가격 / 총 수량
+        avgNummArr[i] = Math.floor(priNumArr[i] / buyNumArr[i]);
+        avg[i + 1].innerHTML = avgNummArr[i];
+
+        // if(buyNumArr[i] == 0){
+        //     priNumArr[i].innerHTML=0;
+        //     plusArr[i].innerHTML=0;
+
+        // }
+        console.log(priNumArr[i] );  // 총 매입가격?>
+        console.log(avgNummArr[i]);  // 평균단가??
+        console.log(plusArr[i]);
+        // 평가손익 (보유중인 주식의 현재가격 * 수량) - 매입총액 = 평가손익
+        plusArr[i] = (nowPrice[i].innerHTML * buyNumArr[i]) - priNumArr[i]
+        plus[i + 1].innerHTML = plusArr[i];
+
+        // 수익률 (현재가 - 평균매입가격 / 평균매입가격) * 100 = 수익률 
+        percentArr[i] = ((nowPrice[i].innerHTML - avgNummArr[i]) / avgNummArr[i]) * 100
+        plus[i + 1].innerHTML = `${plusArr[i]} (${percentArr[i].toFixed(2)} %)`// 소수점 2자리 까지 : toFixed(2)
+
+        
+
+        // 총평가금액
+        allMonArr[i] = (priNumArr[i] + plusArr[i])
+        allMon[i + 1].innerHTML = allMonArr[i];
+    
+
+        console.log(priNumArr[i] );  // 총 매입가격?>
+        console.log(avgNummArr[i]);  // 평균단가??
+        console.log(plusArr[i]); // 평가손익
+        console.log( percentArr[i]); // 수익률
+        console.log(allMonArr[i]); // 총 평가금액
+        console.log(buyNumArr[i]); //수량
+    }
+    else {
+        alert("돈 없어")
+    }
+}
+// 평가손익, 총평가금액은 매수매도 했을대도 바꿔지고 라운드가 끝났을때도 바꿔주기
+
+// 매도 : 
+
+// 보유현금 + 현재가격
+// 수량 - 현재수량
+// 총 매입가격 - 현재가격
+// 평군단가 : (총매입가격-현재가격 / 총 수량-현재수량)
+
+// 평가손익 , 수익률, 총평가금액은 그대로
+
+// 조건 : 매도할때 (현재가 * 수량) <= 총평가금액
+function setvalueInText(i) {
+
+    // 함수로 만들기
+    let inputData = document.getElementsByClassName("data")[i].value; // 내가 입력한 매도 개수
+    if (inputData <= 0) {
+        alert("1이상의 숫자만 넣어주세요")
+        // console.log(inputNum.value);
+        document.getElementsByClassName("data")[i].value = ""
+    } else if (!Number.isInteger(parseFloat(inputData))) { //Number.isInteger() 함수는 입력된 값이 정수인지를 체크 // parseFloat() 함수는 문자열을 부동소수점 수로 변환
+        alert("소수점 안돼")
+        document.getElementsByClassName("data")[i].value = ""
+    }
+
+    else if (allMonArr[i] >= nowPrice[i].innerHTML * inputData) { // 현재가 * 매도 하는 수량 이 내 평가금액보다 낮을 때만 매도가 가능하지 
+
+        
+        // 보유현금
+        allMonArr[i] -= nowPrice[i].innerHTML * inputData // 총 평가금액에서 
+        allMon[i + 1].innerHTML = allMonArr[i];
+
+        // 지갑
+        money = money + (nowPrice[i].innerHTML * inputData)
+        wallet.innerHTML = `내 보유현금 : ${money} 원`
+
+        // 수량 (수량 - 현재수량)
+        buyNumArr[i] -= Number(inputData); // 
+        text[i + 1].innerHTML = buyNumArr[i] // 수량 자리에 표시
+        
+       
+
+        // 총 매입가격 (총 매입가격 - 현재가격)
+        priNumArr[i] -= nowPrice[i].innerHTML * inputData; 
+        if( buyNumArr[i]==0){
+            priNumArr[i]=0;
+        }
+        buyPrice[i + 1].innerHTML = priNumArr[i]
+
+        // 평균단가 총매입가격 / 총 수량
+        if (buyNumArr[i] !== 0) { // avgNummArr[i]의 결과값이 0이 되면 평균을 계산할 수 없기 때문에 NaN(Not a Number)이 출력
+            avgNummArr[i] = Math.floor(priNumArr[i] / buyNumArr[i]);
+            avg[i + 1].innerHTML = avgNummArr[i];
+        } else {
+            avgNummArr[i] = 0;
+            avg[i + 1].innerHTML = avgNummArr[i];
+        }
+
+        // 평가손익 (보유중인 주식의 현재가격 * 수량) - 매입총액 = 평가손익
+        plusArr[i] = (nowPrice[i].innerHTML * buyNumArr[i]) - priNumArr[i]
+        plus[i + 1].innerHTML = plusArr[i];
+
+        // 수익률 (현재가 - 평균매입가격 / 평균매입가격) * 100 = 수익률 
+        if (avgNummArr[i] !== 0) { //분모가 0일때 무한으로 출력이됨 방지해줌
+            percentArr[i] = ((nowPrice[i].innerHTML - avgNummArr[i]) / avgNummArr[i]) * 100
+            plus[i + 1].innerHTML = `${plusArr[i]} (${percentArr[i].toFixed(2)} %)`// 소수점 2자리 까지 : toFixed(2)
+        } else {
+            percentArr[i] = 0;
+        }
+
+        // 총평가금액
+        allMonArr[i] = (priNumArr[i] + plusArr[i])
+        allMon[i + 1].innerHTML = allMonArr[i];
+
+        console.log(priNumArr[i] );  // 총 매입가격?>
+        console.log(avgNummArr[i]);  // 평균단가??
+        console.log(plusArr[i]); // 평가손익
+        console.log( percentArr[i]); // 수익률
+        console.log(allMonArr[i]); // 총 평가금액
+        console.log(buyNumArr[i]); //수량
+    
+        
+    }
+    else {
+        alert("돈 없어") 
+    }
+}
 
 
 
